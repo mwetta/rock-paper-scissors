de5355663f78bf0ebaa21ecba673727938afb6b6
Madeline <moon.madeline@gmail.com>
2022-06-02 15:22:02 -0500

    Outline pseudocode for rock paper scissors

---
+++ script.js
@@ -0,0 +1,29 @@
+/*  computerPlay
+        randomly select from rock, paper, scissors
+        store in computerSelection
+
+    console.log(computerSelection)
+
+    userPlay prompts for input either rock, paper, scissors // should be case insensitive
+        store in playerSelection 
+
+    if playerSelection != rock, paper, scissors
+        return error 
+
+    else
+
+    playRound
+        flag player win if
+            playerSelection = rock && computerSelection = scissors 
+            OR
+            playerSelection = paper && computerSelection = rock
+            OR 
+            playerSelection = scissors && computerSelection = paper
+        
+        if win 
+            return "You win! " + playerSelection + " beats " + computerSelection + "!"
+            
+        else
+            return "You lose! " + computerSelection + " beats " + playerSelection + "!"
+
+*/
