6dccb7ba46753defdc779827e8acfccafb827e1c
Madeline <moon.madeline@gmail.com>
2022-06-04 12:45:58 -0500

    Correct comparison operators and add 'draw'
    Correct comparison operators from assignments in a facepalm moment.
    Add a check for draw if playerSelection and computerSelection are
    the same.

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-const computerPlay = Math.floor(Math.random() * choices.length);
-let computerSelection = computerPlay;
-console.log(choices[computerPlay]); 
+let computerPlay = Math.floor(Math.random() * choices.length);
+let computerSelection = (choices[computerPlay]);
+
 
 let userPlay = prompt('Rock, Paper, Scissors?', ''); 
 let playerSelection = userPlay.toLowerCase();
-let validOption = (choices.includes(userPlay)) ? true : false; //checks to see whether userPlay contains something from choices Array, returns true or false and stores in validOption
-
-console.log(choices.includes(userPlay));
-console.log(validOption);
-console.log(userPlay);
+let validOption = (choices.includes(playerSelection)) ? true : false; //checks to see whether userPlay contains something from choices Array, returns true or false and stores in validOption
 
 /* playRound returns true if the player wins; else it returns false
     does not deal with "draws" - if both are the same
     need to add messages
 */
-
+let win = 'false';
+let draw = 'false';
 game();
 
 function game()
 {
-   function playRound(playerSelection, computerSelection)
+   function playRound()
     { 
-        if ('rock','scissors') {
-            return true;
-        } else if ('scissors', 'paper') {
-            return true;
-        } else if ('paper', 'rock'){
-            return true;
-        }   else {
-            return false;
+        
+        if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            win = 'true';
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            win = 'true';
+        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+            win = 'true';
+        }   else if (playerSelection === computerSelection){
+            draw = 'true';
+        } else {
+            win = 'false';
         };
     }
+    playRound();
 
-    if (playRound = 'true') {
-        alert(`You win! ${playerSelection} beats ${choices[computerPlay]}!`);
-        console.log(`You win! ${playerSelection} beats ${choices[computerPlay]}!`);
-    } else {
-        alert(`You lose! ${choices[computerPlay]} beats ${playerSelection}!`);
-        console.log(`You lose! ${choices[computerPlay]} beats ${playerSelection}!`);
+    if (win === 'true') {
+        console.log(`You win! ${playerSelection} beats ${choices[computerPlay]}!`)
+    } else if (draw === 'true') {
+        console.log(`No one wins! You played ${playerSelection} and the computer played ${choices[computerPlay]}! It is a draw!`)
+    } else if (win === 'false') {
+        console.log(`You lose! ${choices[computerPlay]} beats ${playerSelection}!`)
     }
 }
 
+console.log(computerSelection); 
+console.log(choices.includes(userPlay));
+console.log(validOption);
+console.log(playerSelection);
+console.log('player win =' + win);
     /* if userPlay is null, call userPlay
 
-        if validOption = falsse, call userPlay
+        if validOption = false, call userPlay
 
         else playRound
     */
